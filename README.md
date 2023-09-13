e50bea673d59fe7085805f0baccd365e1c8a42ae
gmcdowell <greg@oxigeno.co.nz>
2023-09-13 13:39:23 +1200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# local_notifications_example
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/local_notifications_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.local_notifications_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
